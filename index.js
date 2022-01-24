70bf0133e07e9aab2c11e4890f4e648e8614c174
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-24 23:18:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,6 @@ var removeElement = function(nums, val) {
     return  j
     
 };
+console.log(removeElement([0,1,2,2,3,0,4,2],  2) )
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Output: 5, nums = [0,1,4,0,3,_,_,_]
