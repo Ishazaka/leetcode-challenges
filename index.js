3f768a13d41ce938eec99eedbb7c50f519cae403
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-03-14 08:31:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ var removeElement = function(nums, val) {
  console.log(removeElement([0,1,2,2,3,0,4,2],  2) )
 
 
+// compare the triplets
+
+ var ap = [3, 5, 2];
+ var bp = [2, 5, 4];
+
+function compareTriplets(a, b) {
+    // Write your code here
+   let apoints = 0;
+   let bpoints = 0;
