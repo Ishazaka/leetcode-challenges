0508410bb4a888bdd95c15b70f21aa83368c2f17
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-03-17 09:02:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ console.log(toNumberArray(["9.4", "4.2"]))
 // example => tuckIn([15,150], [45, 75, 35]) ➞ [15, 45, 75, 35, 150]
 // example => tuckIn([[1, 2], [5, 6]], [[3, 4]]) ➞ [[1, 2], [3, 4], [5, 6]]
 
-
+// first method
 function tuckIn(arr1, arr2) {
 	 arr2.push(arr1[1]);
 	arr2.unshift(arr1[0]);
