3de457a318a113a01c6abb3dfee2501dd401b0c6
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-03-18 04:33:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -80,3 +80,7 @@ function tuckIn(arr1, arr2) {
 for(let i = 0; i <= 100; i++){
  if(i % 3 == 0 && i % 5 == 0){
    console.log("FizzBuzz");
+   }else if(i % 3 == 0){
+   console.log("fizz");
+ }else if(i % 5 == 0){
+   console.log("buzz");
