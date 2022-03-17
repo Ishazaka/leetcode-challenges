2ab41dbbe30b33af98492e1136ad3ad48a643357
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-03-17 09:01:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -59,4 +59,5 @@ console.log(toNumberArray(["9.4", "4.2"]))
 
 
 // Create a function that takes two arrays and insert the second array in the middle of the first array.
-
+// example => tuckIn([15,150], [45, 75, 35]) ➞ [15, 45, 75, 35, 150]
+// example => tuckIn([[1, 2], [5, 6]], [[3, 4]]) ➞ [[1, 2], [3, 4], [5, 6]]
