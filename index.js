683fa90f73f8e976ee99f3e5d4ee1c8a57f63ad5
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-03-19 15:55:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -99,3 +99,5 @@ for(let i = 0; i <= 100; i++){
 const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
 const sharky = ["Sharky", "shark", 20, "Left Coast"]
 const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
