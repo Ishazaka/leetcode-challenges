697b809a6fa2e9514fdf2a5d3d92b2f05a85bf63
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-04-16 06:50:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -255,3 +255,6 @@ console.log(toBinary(0xFF));
 
 
 14--  // // Given a number, return an array containing the two halves of the number. If the number is odd, make the rightmost number higher.
+ // numberSplit(10) ➞ [5, 5]
+ // numberSplit(11) ➞ [5, 6]
+// numberSplit(-9) ➞ [-5, -4]
